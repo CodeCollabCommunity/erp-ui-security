2bd4fa4ea32777cee0f4a20408fa79e96db6b193
MoroniAg <moroniaguilera1993@gmail.com>
2024-04-25 21:36:51 -0500

    F #3 - add tailwind confguration

--- app/home/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Home() {
-    return (
-        <span>ID</span>
-    )
-}
--- app/home/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Home() {
-    return (
-        <span>Hola mundo</span>
-    )
-}
